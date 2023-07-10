258ba8eb7f47138b5ae6408de84f88b28ced3621
Jose Daniel Romero <danyrome12@gmail.com>
2023-07-09 19:53:38 -0500

    Finalizado el desarrollo del home page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,17 +3,20 @@ import { NavLink } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <nav>
+    <>
+      <nav>
         <div className='logo'></div>
         <div className='navbar-container'>
-            <ul>
-                <li><NavLink to='/' className='nav-item'>00 HOME</NavLink></li>
-                <li><NavLink to='/destination' className='nav-item'>01 DESTINATION</NavLink></li>
-                <li><NavLink to='/destination' className='nav-item'>02 CREW</NavLink></li>
-                <li><NavLink to='/destination' className='nav-item'>03 TECHNOLOGY</NavLink></li>
-            </ul>
+          <ul>
+            <li><NavLink to='/' className='nav-item'>00 HOME</NavLink></li>
+            <li><NavLink to='/destination' className='nav-item'>01 DESTINATION</NavLink></li>
+            <li><NavLink to='/destination' className='nav-item'>02 CREW</NavLink></li>
+            <li><NavLink to='/destination' className='nav-item'>03 TECHNOLOGY</NavLink></li>
+          </ul>
         </div>
       </nav>
+      <hr></hr>
+    </>
   )
 }
 
